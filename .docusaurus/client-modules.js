986f21d897a3380082de7a71de7df37bd870d065
chuipagro <pablo.levy@epitech.eu>
2025-10-08 16:05:42 +0200

    add .docusaurus folder

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/home/chuipagro/epitech/pedago/RTFM/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/home/chuipagro/epitech/pedago/RTFM/node_modules/infima/dist/css/default/default.css"),
+  require("/home/chuipagro/epitech/pedago/RTFM/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/chuipagro/epitech/pedago/RTFM/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/chuipagro/epitech/pedago/RTFM/src/css/custom.css"),
+];
